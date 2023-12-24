ace02be3b688e67ccca23d66e7718632c28b1400
Fatthallah <eng.fatthallah@gmail.com>
2023-12-23 18:01:58 -0800

    x

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-
+""" FizzBuzz
+    Change of logic if (i % 3) == 0 and (i % 5) == 0:
 """
-    Comment
-    the comment I have to read
-"""
-
 import sys
 
 
 def fizzbuzz(n):
+    """
+    FizzBuzz function prints numbers from 1 to n separated by a space.
     
-	"""
-        the comment I have to read
-        the comment I have to read
-        """
+    - For multiples of three print "Fizz" instead of the number and for
+      multiples of five print "Buzz".
+    - For numbers which are multiples of both three and five print "FizzBuzz".
+    """
     if n < 1:
         return
 
